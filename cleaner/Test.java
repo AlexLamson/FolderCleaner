093cc33a911a726168151a7603ed76bacb1e6ed2
Alex <alex.lamson@gmail.com>
2013-12-13 21:23:39 -0500

    Fixed test path

--- cleaner/Test.java
+++ cleaner/Test.java
@@ -12,7 +12,7 @@ public class Test
 	
 	public static void main(String[] args)
 	{
-		printAttributes(new File("C:\\Users\\LamsonFamily\\Desktop\\Fallacies.jpg"));
+		printAttributes(new File("Lists\\ExampleBlacklist.txt"));
 	}
 	
 	public static void killProcess(String process)
